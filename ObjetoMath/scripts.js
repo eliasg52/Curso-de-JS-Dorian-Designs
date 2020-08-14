7180e5f90e69f1ed877c00148f6fae73dcfc029f
eliasg52 <eliasgarcia81@gmail.com>
2020-08-13 21:16:57 -0300

    Condicionales, Objeto Math y Strings

---
+++ ObjetoMath/scripts.js
@@ -0,0 +1,43 @@
+/*
+
+Objeto Math
+
+Objeto estático
+
+Tenemos que usar su nombre para utilizarlo
+
+Propiedades:
+    Math.E - Math.Pi
+Métodos:
+    Math.abs(x) Devuelve el valor absoluto de x
+    Math.ceil(x) Devuelve el entero más grande mayor o igual que un número.
+    Math.floor(x) Devuelve el entero más pequeño menor o igual que un número.
+    Math.pow(x, y) Devuelve la potencia de x elevado a y
+    Math.sqrt(x) Devuelve la raiz cuadrada de x
+    Math.random() Genera un número pseudoaleatorio entre 0 y 1
+    Math.round(x) Devuelve el valor de un número redondeado al entero más cercano.
+    Math.sign(x) Devuelve el signo de la x, que indica si x es positivo, negativo o cero.
+*/
+
+console.log(Math.E);
+console.log(Math.PI);
+
+let num = 5.3;
+
+console.log(Math.abs(num));
+
+console.log(Math.ceil(num));
+
+console.log(Math.floor(num));
+
+console.log(Math.pow(num, 2));
+
+/* numero aleatorio entre 1 y 100 */
+console.log(Math.round(Math.random() * 100));
+
+/* formula para generar numeros aleatorios entre un numero y otro */
+/* console.log(Math.random() * (max-min)+min) */
+
+console.log(Math.round(Math.random() * (100 - 50) + 50));
+
+console.log(Math.sign(num));
---
+++ Variables/scripts.js
@@ -0,0 +1,38 @@
+/* VARIABLES*/
+
+/* let palabra = 'Hola mundo';
+
+console.log(palabra);
+
+let respuesta = true;
+
+console.log(respuesta);
+
+const PI = 3.14;
+
+console.log(PI); */
+
+/* OPERADORES */
+
+/* let a = 5;
+let b = 2;
+let suma = a + b;
+
+console.log(suma);
+
+let usuario = 'Dorian';
+
+console.log('Hola ' + usuario); */
+
+/* operador incremento */
+/* let a = 5;
+console.log(a++);
+console.log(a);
+console.log(++a); */
+
+/* operador asignacion */
+
+/* let a = 10;
+a += 3;
+
+console.log(a); */
