1a785e8d98a56b6393e8d92b9b72350b584ce353
eliasg52 <eliasgarcia81@gmail.com>
2020-08-15 20:22:13 -0300

    Switch, Arrays y Operador Ternario

---
+++ Estructura Switch/scripts.js
@@ -0,0 +1,61 @@
+/*
+Sintaxis Simple
+
+switch(evaluación) {
+    case n1: 
+        //código
+    break;
+
+    case n2:
+        //código
+    break;
+    default:
+         //código
+}
+
+Sintaxis Múltiple
+
+switch(evaluación) {
+    case n1:
+    case n2:
+    case n3:
+    case n4:
+        //código
+    break;
+
+    case n5:
+    case n6:
+        //código
+    break;
+    default:
+         //código
+} 
+*/
+
+let num = 2;
+
+switch (num) {
+  case 1:
+    console.log('El numero vale 1');
+    break;
+  case 2:
+    console.log('El numero vale 2');
+    break;
+  default:
+    console.log('El numero no vale 1 ni 2');
+    break;
+}
+
+switch (num) {
+  case 1:
+  case 3:
+  case 5:
+    console.log(`${num} es impar`);
+    break;
+  case 2:
+  case 4:
+    console.log(`${num} es par`);
+    break;
+  default:
+    break;
+}
