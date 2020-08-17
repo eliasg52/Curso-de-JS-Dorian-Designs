7e17b9692ea5b40b1961ff8fcd93f24220df5d6b
eliasg52 <eliasgarcia81@gmail.com>
2020-08-17 01:48:19 -0300

    Arrays2, bucles for,while,dowhile

---
+++ Bucle For of, For in/scripts.js
@@ -0,0 +1,26 @@
+/* Bucles */
+
+/* 
+    Determinados
+
+    bucle for of / for in
+
+    for(let variable of estructura){
+        Código a ejecutar
+    }
+
+    Palabras reservadas de bucles
+
+        break - Rompe el bucle
+        continue - Se salta la(s) posicion(es) que le indequemos y después continúa su ejecución
+*/
+
+let names = ['Paco', 'José', 'Paula', 'María'];
+
+for (let name of names) {
+  console.log(name);
+}
+
+for (let index in names) {
+  console.log(index);
+}
