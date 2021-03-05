0c51567725f0bf6bef9b4ce64db9bdb37b274efe
eliasg52 <eliasgarcia81@gmail.com>
2021-03-05 11:22:42 -0300

    Update scripts.js

--- Operador Spread/scripts.js
+++ Operador Spread/scripts.js
@@ -9,6 +9,18 @@ Su sintaxis es ...
 
 //Enviar elementos en un array a una función
 
+const sumarNumeros = (a, b, c) => {
+  console.log(a + b + c);
+};
+
+sumarNumeros(3, 4, 5);
+
+let numerosParaSumar = [3, 4, 6];
+//b y c van a ser undefined. ya que A seria el 3,4 y 5
+sumarNumeros(numerosParaSumar);
+
+sumarNumeros(...numerosParaSumar);
+
 /* const sumarNumeros = (a, b, c, d) => {
   console.log(a + b + c + d);
 };
@@ -24,9 +36,9 @@ let usuarios = ['pepe', 'julieta', 'colo', 'rosa', 'julian'];
 let nuevosUsuarios = ['marta', 'jaime', 'laura'];
 
 //ESTA SERIA LA MANERA MAS 'ENGORROSA' DE AGREGAR LOS NUEVOS USUARIOS AL ARRAY USUARIOS
-/* usuarios.push(nuevosUsuarios[0], nuevosUsuarios[1], nuevosUsuarios[2]);
+usuarios.push(nuevosUsuarios[0], nuevosUsuarios[1], nuevosUsuarios[2]);
 console.log(usuarios);
- */
+
 //LA MANERA MAS FACIL CON EL SPREAD OPERATOR ES ASI:
 usuarios.push(...nuevosUsuarios);
 console.log(usuarios);
@@ -40,27 +52,27 @@ console.log(array2); */
 
 //Concatenar arrays
 
-/* let array1 = [1, 2, 3, 4, 5];
-let array2 = [6, 7, 8]; */
+let array1 = [1, 2, 3, 4, 5];
+let array2 = [6, 7, 8];
 
-/* //FORMA DE CONCATENAR CON CONCAT
- let arrayConcatenado = array1.concat(array2); */
-
-//FORMA DE CONCATENAR CON SPREAD OPERATOR
-/* let arrayConcatenado = [...array1, ...array2];
-
+//FORMA DE CONCATENAR CON CONCAT
+/* let arrayConcatenado = array1.concat(array2);
 console.log(arrayConcatenado); */
+//FORMA DE CONCATENAR CON SPREAD OPERATOR
+let arrayConcatenado = [...array1, ...array2];
+
+console.log(arrayConcatenado);
 
 // Enviar un número indefinido de argumentos a una función (parámetros REST)
 
-/* const parametrosRest = (...numeros) => {
+const parametrosRest = (...numeros) => {
   console.log(numeros);
 };
 
-parametrosRest(1, 2, 3, 4, 5, 6); */
+parametrosRest(1, 2, 3, 4, 5, 6);
 
 //librería math
-
+/* 
 const numeros = [-1, 1, 2, 3, 4, 5, 6, 7, 2, 4, 5];
 
 console.log(Math.max(...numeros));
@@ -72,3 +84,8 @@ console.log(new Set(numeros));
 //Nuevo array con elementos sin repetir
 const sinRepetir = [...new Set(numeros)];
 console.log(sinRepetir);
+ */
+
+let numeros = [23, 43, 2, 67, 4];
+
+console.log(...numeros);
